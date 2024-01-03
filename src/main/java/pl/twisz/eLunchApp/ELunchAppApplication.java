fc001eabc166c83646440cd2491671ea97e179f7
twisz <notarealemail@mail.com>
2024-01-03 12:59:49 +0100

    Initial commit

---
+++ src/main/java/pl/twisz/eLunchApp/ELunchAppApplication.java
@@ -0,0 +1,13 @@
+package pl.twisz.eLunchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELunchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELunchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/twisz/eLunchApp/ELunchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.twisz.eLunchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELunchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
