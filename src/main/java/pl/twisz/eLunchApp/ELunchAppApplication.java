ea4d53df181cc5a8df03aa406efc58a7a18bfad6
twiszynski <tomaszwiszynski@gmail.com>
2024-03-16 00:19:14 +0200

    EnableCaching annotation in main App class

--- src/main/java/pl/twisz/eLunchApp/ELunchAppApplication.java
+++ src/main/java/pl/twisz/eLunchApp/ELunchAppApplication.java
@@ -2,8 +2,10 @@ package pl.twisz.eLunchApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ELunchAppApplication {
 
 	public static void main(String[] args) {
